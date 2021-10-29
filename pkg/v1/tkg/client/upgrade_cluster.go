cec685c6b53796fe7bd072baa55e72a950eb7782
Harish Yayi <yharish991@gmail.com>
2021-10-29 10:33:27 -0400

    add CRS for management package repository
    Signed-off-by: Harish Yayi <yharish991@gmail.com>

--- pkg/v1/tkg/client/upgrade_cluster.go
+++ pkg/v1/tkg/client/upgrade_cluster.go
@@ -349,6 +349,7 @@ func (c *TkgClient) upgradeAddonPreNodeUpgrade(regionalClusterClient clusterclie
 			"addons-management/tanzu-addons-manager",
 			"tkr/tkr-controller",
 			"addons-management/core-package-repo",
+			"packages/management-package-repo",
 			"capabilities/capabilities-controller")
 	}
 	upgradeClusterMetadataOptions := &UpgradeAddonOptions{
